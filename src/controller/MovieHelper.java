e5d5f8404af2ebd3524d6572a5c6bfba0cc292f6
rddeuser <bdeuser13@gmail.com>
2021-10-07 21:54:16 -0500

    Add Movie and MovieHelper classes

---
+++ src/controller/MovieHelper.java
@@ -0,0 +1,14 @@
+/**
+ * @author Becca Deuser - rddeuser
+ * CIS175 - Fall 2021
+ * Oct 7, 2021
+ */
+package controller;
+
+/**
+ * @author bdeus
+ *
+ */
+public class MovieHelper {
+
+}
---
+++ src/model/Movie.java
@@ -0,0 +1,14 @@
+package model;
+/**
+ * @author Becca Deuser - rddeuser
+ * CIS175 - Fall 2021
+ * Oct 7, 2021
+ */
+
+/**
+ * @author bdeus
+ *
+ */
+public class Movie {
+
+}
