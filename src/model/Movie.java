1d4287c689254a45c61764be1a71407743a904b5
rddeuser <bdeuser13@gmail.com>
2021-10-13 12:38:35 -0500

    Update Movie.java
    Added attributes, constructors, getters and setters to Movie.java

--- src/model/Movie.java
+++ src/model/Movie.java
@@ -1,4 +1,7 @@
 package model;
+
+import java.time.LocalDate;
+
 /**
  * @author Becca Deuser - rddeuser
  * CIS175 - Fall 2021
@@ -9,6 +12,69 @@ package model;
  * @author bdeus
  *
  */
-public class Movie {
 
-}
+public class Movie {
+	//declare attributes
+	private int id;
+	private String title;
+	private String genre;
+	private LocalDate releaseDate;
+	private double rating;
+	
+	public Movie() {
+		
+	}//end default constructor
+
+	public Movie(String title, String genre, LocalDate releaseDate, double rating) {
+		this.title = title;
+		this.genre = genre;
+		this.releaseDate = releaseDate;
+		this.rating = rating;
+	}//end all attribute constructor
+
+	public Movie(String title, String genre) {
+		this.title = title;
+		this.genre = genre;
+	}//end 2 attribute constructor
+
+	public Movie(String title) {
+		this.title = title;
+	}//end constructor
+
+	public String getTitle() {
+		return title;
+	}//end getTitle
+
+	public void setTitle(String title) {
+		this.title = title;
+	}//end setTitle
+
+	public String getGenre() {
+		return genre;
+	}//end getGenre
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}//end setGenre
+
+	public LocalDate getReleaseDate() {
+		return releaseDate;
+	}//end getReleaseDate
+
+	public void setReleaseDate(LocalDate releaseDate) {
+		this.releaseDate = releaseDate;
+	}//end setReleaseDate
+
+	public double getRating() {
+		return rating;
+	}//end getRating
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}//end setRating
+	
+	public int getId() {
+		return id;
+	}//end getId
+	
+}//end Movie
