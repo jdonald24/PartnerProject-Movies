91072c8d7540ccaa26bccbb5814c215c2f615b00
jdonald24 <endershadow128@gmail.com>
2021-10-12 19:57:58 -0500

    added the Theater and TheaterHelper classes

---
+++ src/controller/TheaterHelper.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * @author Austin Donald - ajdonald
+ * CIS175 - Fall 2021
+ * Oct 12, 2021
+ */
+public class TheaterHelper {
+
+}
---
+++ src/model/Theater.java
@@ -0,0 +1,86 @@
+package model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ * @author Austin Donald - ajdonald
+ * CIS175 - Fall 2021
+ * Oct 12, 2021
+ */
+@Entity
+public class Theater {
+	@Id
+	@GeneratedValue
+	private int id;
+	@OneToMany(cascade=CascadeType.PERSIST)
+	private List<Movie> listOfMovies;
+	private String theaterName;
+	private String theaterLocation;
+	public Theater () {
+		
+	}
+	public Theater (String theaterName, String theaterLocation, List<Movie> listOfMovies) {
+		this.theaterName = theaterName;
+		this.theaterLocation = theaterLocation;
+		this.listOfMovies = listOfMovies;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the listOfMovies
+	 */
+	public List<Movie> getListOfMovies() {
+		return listOfMovies;
+	}
+	/**
+	 * @param listOfMovies the listOfMovies to set
+	 */
+	public void setListOfMovies(List<Movie> listOfMovies) {
+		this.listOfMovies = listOfMovies;
+	}
+	/**
+	 * @return the theaterName
+	 */
+	public String getTheaterName() {
+		return theaterName;
+	}
+	/**
+	 * @param theaterName the theaterName to set
+	 */
+	public void setTheaterName(String theaterName) {
+		this.theaterName = theaterName;
+	}
+	/**
+	 * @return the theaterLocation
+	 */
+	public String getTheaterLocation() {
+		return theaterLocation;
+	}
+	/**
+	 * @param theaterLocation the theaterLocation to set
+	 */
+	public void setTheaterLocation(String theaterLocation) {
+		this.theaterLocation = theaterLocation;
+	}
+	@Override
+	public String toString() {
+		return "Theater [id=" + id + ", listOfMovies=" + listOfMovies + ", theaterName=" + theaterName
+				+ ", theaterLocation=" + theaterLocation + "]";
+	}
+}
